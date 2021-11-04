0986bea290655aa73fbe6cc94d913f79d1254ed7
stefanforce <46653491+stefanforce@users.noreply.github.com>
2021-11-04 09:36:58 +0200

    Delete Lab4 directory

--- Lab4/buildAndRun.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call mvn clean package
-call docker build -t com.mycompany/Lab3 .
-call docker rm -f Lab3
-call docker run -d -p 9080:9080 -p 9443:9443 --name Lab3 com.mycompany/Lab3
--- Lab4/src/main/java/com/mycompany/lab3/JAXRSConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.lab3;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("resources")
-public class JAXRSConfiguration extends Application {
-
-}
